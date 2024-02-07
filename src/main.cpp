08bb9259c39ffbcb11fd84853cc01185dcbbc840
MarionBarthe <marion.barthe@laposte.net>
2024-02-07 14:05:35 +0100

    v0.1 : des points se déplacent de façon aléatoire dans la fenêtre

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,42 @@
 #include "doctest/doctest.h"
 #include "p6/p6.h"
 
+struct Boid {
+    glm::vec2 position;
+    glm::vec2 velocity;
+    float radius = 0.05;
+
+    Boid()
+        : position(p6::random::point()), velocity(p6::random::point(glm::vec2(0., 0.), glm::vec2(0.01, 0.01))){};
+};
+
+void updateBoid(p6::Context* ctx, Boid* b)
+{
+    b->position += b->velocity;
+    if (b->position.x < -ctx->aspect_ratio())
+        b->position.x = ctx->aspect_ratio();
+    if (b->position.y < -1)
+        b->position.y = 1;
+    if (b->position.x > ctx->aspect_ratio())
+        b->position.x = -ctx->aspect_ratio();
+    if (b->position.y > 1)
+        b->position.y = -1;
+};
+
+void drawBoid(p6::Context* ctx, Boid* b)
+{
+    ctx->fill = {1.f, 0.7f, 0.2f};
+    ctx->circle(p6::Center{b->position}, p6::Radius{b->radius});
+}
+
 int main()
 {
+    std::vector<Boid> boids;
+    for (int i = 0; i < 10; i++)
+    {
+        boids.emplace_back(Boid());
+    }
+
     // Run the tests
     if (doctest::Context{}.run() != 0)
         return EXIT_FAILURE;
@@ -16,10 +50,11 @@ int main()
     // Declare your infinite update loop.
     ctx.update = [&]() {
         ctx.background(p6::NamedColor::Blue);
-        ctx.circle(
-            p6::Center{ctx.mouse()},
-            p6::Radius{0.2f}
-        );
+        for (auto& b : boids)
+        {
+            drawBoid(&ctx, &b);
+            updateBoid(&ctx, &b);
+        }
     };
 
     // Should be done last. It starts the infinite loop.
