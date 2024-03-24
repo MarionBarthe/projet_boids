a4725288167835e9d6d318e6848993b3205133a9
Guilhem Duval <78139983+GuilhemDuval@users.noreply.github.com>
2024-03-24 21:53:39 +0100

    v1.91 : ajout de la distribution exponentielle et de la distribution laplace
    + static_cast

--- src/random_generator.cpp
+++ src/random_generator.cpp
@@ -32,7 +32,7 @@ unsigned long long binomial_coefficient(int n, int k)
 // Simulate bernoulli distribution with p, probability of success
 bool bernoulli_distribution(double p)
 {
-    double random_num = (double)rand() / RAND_MAX; // Random number between 0 and 1
+    double random_num = static_cast<double>(rand()) / RAND_MAX; // Random number between 0 and 1
 
     return random_num < p; // Return 0 or 1 depending on the success
 }
@@ -46,7 +46,7 @@ int binomial_distribution(int n, double p)
     for (int i = 0; i <= n; i++)
     {
         // Cumulative probability of obtaining i success in the sequence of trials
-        success_probability += (double)binomial_coefficient(n, i) * pow(p, i) * pow(1 - p, n - i);
+        success_probability += static_cast<double>(binomial_coefficient(n, i)) * pow(p, i) * pow(1 - p, n - i);
 
         // Check if the generated threshold is lower than the cumulative probability
         if (random_threshold < success_probability)
@@ -55,4 +55,27 @@ int binomial_distribution(int n, double p)
         }
     }
     return -1;
+}
+
+// Simulate exponential distribution by generating a time quotient
+int exponential_distribution(double lambda)
+{
+    double random_exp = -log(static_cast<double>(rand()) / RAND_MAX) / lambda; // Random number between 0 and 1
+
+    double random_scaled = random_exp * 9 + 1;     // Scale the random number to 1 - 10
+    return static_cast<int>(round(random_scaled)); // Round and return the result
+}
+
+// Simulate Laplace distribution with parameters mu, location and b, scale
+double laplace_distribution(double mu, double b)
+{
+    double u = static_cast<double>(rand()) / RAND_MAX - 0.5; // Random number between -0.5 and 0.5
+
+    if (u < 0)
+    {
+        // If u is less than 0, use the left side of the Laplace distribution
+        return mu + b * log(1 - 2 * u);
+    }
+    // If u is greater than or equal to 0, use the right side of the Laplace distribution
+    return mu - b * log(1 - 2 * u);
 }
